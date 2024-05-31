2c2d0943f4ae4cea28c869dddc05c1c7236f531e
Tom Gao <tom@tomgao.cc>
2024-05-31 22:22:03 +0800

    chore: optimize example to support i18n-ally

--- examples/vscode-i18n/tsup.config.ts
+++ examples/vscode-i18n/tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig(options => {
   return {
     entry: ['src/index.ts'],
     format: ['cjs'],
-    target: ['es2020', 'node14'],
+    target: 'node14',
     external: ['vscode'],
     clean: true,
     splitting: true,
