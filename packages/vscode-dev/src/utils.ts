134c28393aef751061a874144e82161c43c92769
Tom Gao <tom@tomgao.cc>
2026-01-02 14:49:05 +0800

    fix: gen vscode code

--- packages/vscode-dev/src/utils.ts
+++ packages/vscode-dev/src/utils.ts
@@ -6,47 +6,6 @@ import { mkdirpSync } from '@tomjs/node';
 
 export const logger = new Logger({ directory: 'vscode-dev/logs' });
 
-export async function formatCode(code: string, cwd: string) {
-  try {
-    const prettier = await import('prettier');
-
-    let filepath: string | undefined;
-    const configFileNames = ['.prettierrc', '.prettier.cjs', '.prettier.mjs', '.prettier.js'];
-    for (const name of configFileNames) {
-      const fp = path.join(cwd, name);
-      if (fs.existsSync(fp)) {
-        filepath = fp;
-        break;
-      }
-    }
-
-    if (prettier) {
-      return prettier.format(
-        code,
-        filepath
-          ? { filepath }
-          : {
-              parser: 'babel-ts',
-              printWidth: 100,
-              tabWidth: 2,
-              useTabs: false,
-              semi: true,
-              singleQuote: true,
-              trailingComma: 'all',
-              arrowParens: 'avoid',
-              proseWrap: 'never',
-              endOfLine: 'lf',
-            },
-      );
-    }
-  }
-  catch (e: any) {
-    logger.error(e);
-  }
-
-  return code.replace(/^\s+/gm, '').replace(/\n/g, '');
-}
-
 function getDtsDir(cwd: string) {
   const folders = ['types', 'extension', 'src'];
   for (const folder of folders) {
